02bea9a00f0e808c0b7d2bfe597e5770db5cfbf7
jjwoori123-lang <jjwoori123@gmail.com>
2026-01-02 15:26:44 +0900

    programmers only

--- 프로그래머스 완전범죄.py
+++ 프로그래머스 완전범죄.py
@@ -1,3 +1,17 @@
 def solution(info, n, m):
-    answer = 0
-    return answer
+    global answer
+    answer = n
+    visit = set()
+    def dfs(x, a, b):
+        global answer
+        visit.add((x, a, b))
+        if a >= n or b >= m: return
+        if a >= answer: return 
+        if a < answer and x == len(info):
+            answer = a
+            return
+        if (x+1, a + info[x][0], b) not in visit: dfs(x+1, a + info[x][0], b)
+        if (x+1, a, b + info[x][1]) not in visit: dfs(x+1, a, b + info[x][1])    
+    dfs(0,0,0)
+    if answer !=n:return answer
+    elif answer >= n:return -1
